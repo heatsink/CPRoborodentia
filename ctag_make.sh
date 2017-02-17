ccc5a039504f0136b67220628e2da61ffd3b1e75
ssmarok <ssmarok@gmail.com>
2017-02-16 18:24:13 -0800

    Added PWM code for TIM4

---
+++ ctag_make.sh
@@ -0,0 +1,2 @@
+find -L . -regex ".*\.\(h\|c\|hpp\|cpp\)" | xargs gcc -M | sed -e 's/[\\ ]/\n/g' | sed -e '/^$/d' -e '/\.o:[ \t]*$/d' | ctags -L - -I __attribute__,__attribute_deprecated__,__attribute_format_arg__,__attribute_format_strfmon__,__attribute_malloc__,__attribute_noinline__,__attribute_pure__,__attribute_used__,__attribute_warn_unused_result__,__wur,__THROW,__nonnull+ --file-scope=yes --langmap=c:+.h --languages=c,c++ --links=yes --c-kinds=+p --c++-kinds=+p --fields=+liaS --extra=+q
+
