6b8891e501bbe65662f6b38a55c1d6241b5a5719
heatsink <seanswally@gmail.com>
2017-04-20 15:58:18 -0700

    More servo frequency tests

--- Nucleo/Src/main.c
+++ Nucleo/Src/main.c
@@ -791,7 +791,7 @@ static void MX_TIM12_Init(void)
   }
 
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 300;
+  sConfigOC.Pulse = 150;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim12, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
